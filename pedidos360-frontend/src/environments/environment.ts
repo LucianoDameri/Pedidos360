015799aa8b324cac9d86cc12d5e90d3177c3902e
Martin Jaque <tu-correo@duocuc.cl>
2026-09-12 01:43:49 -0300

    Frontend activo

--- pedidos360-frontend/src/environments/environment.ts
+++ pedidos360-frontend/src/environments/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081',
+  apiUrl: 'https://v40douxdrf.execute-api.us-east-1.amazonaws.com/',
   msalConfig: {
     auth: {
       clientId: 'f2e886fe-a3b6-46fe-affe-61b4ea743f2a',
       authority: 'https://login.microsoftonline.com/37e13a6f-33ab-44ae-8cf6-c0f8d4cd40df',
-      redirectUri: 'http://localhost:4200'
+      redirectUri: 'http://localhost:4200/'
     }
   },
   apiConfig: {
     scopes: ['openid', 'profile', 'email'],
-    uri: 'http://localhost:8081'
+    uri: 'https://v40douxdrf.execute-api.us-east-1.amazonaws.com/'
   }
 };
