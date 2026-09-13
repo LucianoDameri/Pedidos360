1a6b387b1eb24b60fff986c2cc2470df2ce2be2c
Martin Jaque <tu-correo@duocuc.cl>
2026-09-13 01:48:48 -0300

    puerto a emviroment

--- pedidos360-frontend/src/environments/environment.ts
+++ pedidos360-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://34.229.246.93:8081',
+  apiUrl: 'http://54.162.136.91:8081',
   msalConfig: {
     auth: {
       clientId: 'f2e886fe-a3b6-46fe-affe-61b4ea743f2a',
@@ -10,6 +10,6 @@ export const environment = {
   },
   apiConfig: {
     scopes: ['openid', 'profile', 'email'],
-    uri: 'http://34.229.246.93:8081'
+    uri: 'http://54.162.136.91:8081'
   }
 };
